9bf1e75e556f3a83213950deddc2f4765e1f84b4
Alexandre <superalexandre0@gmail.com>
2021-11-27 18:10:11 +0100

    [/] Move function

--- src/index.js
+++ src/index.js
@@ -1,78 +1,9 @@
 'use strict'
-const Client = require('./structures/Client')
-const ClientUser = require('./structures/ClientUser')
-const { StickerBuilder } = require("instagram-private-api/dist/sticker-builder/sticker-builder")
-
-ClientUser.prototype.postPhotoInStory = async function(buffer, media) {
-    const sticker = new StickerBuilder()
-        .add(StickerBuilder.attachmentFromMedia((media).center()))
-        .build()
-
-    const story = await this.client.ig.publish.story({
-        file: buffer,
-        stickerConfig: sticker
-    })
-
-    return story
-}
-
-ClientUser.prototype.postStory = async function(buffer) {
-    const story = await this.client.ig.publish.story({
-        file: buffer
-    })
-
-    return story
-}
-
-ClientUser.prototype.postPhoto = async function(buffer, caption, location, usertags) {
-    const media = await this.client.ig.publish.photo({
-        file: buffer,
-        caption: caption ?? "",
-        location: location ?? undefined,
-        usertags: usertags ?? undefined
-    })
-
-    return media
-}
-
-ClientUser.prototype.findLocation = async function(lat, lng, query) {
-    const locations = await this.client.ig.search.location(lat, lng, query ?? "")
-    return locations
-}
-
-ClientUser.prototype.tagUser = async function(users) {
-    async function generateUsertagFromName(ig, name, x, y) {
-        const clamp = (value, min, max) => Math.max(Math.min(value, max), min)
-
-        x = clamp(x, 0.0001, 0.9999)
-        y = clamp(y, 0.0001, 0.9999)
-        const { pk } = await ig.user.searchExact(name)
-        return {
-            user_id: pk,
-            position: [x, y]
-        }
-    }
-
-    if (!Array.isArray(users)) return []
-
-    const usertags = []
-
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i]
-
-        if (!user.username || !user.posX || !user.posY) continue
-
-        const generatedUserTag = await generateUsertagFromName(this.client.ig, user.username, user.posX, user.posY)
-        usertags.push(generatedUserTag)
-    }
-
-    return usertags
-}
 
 module.exports = {
     // Client
-    Client,
-    ClientUser,
+    Client: require('./structures/Client'),
+    ClientUser: require('./structures/ClientUser'),
 
     // Structures
     Attachment: require('./structures/Attachment'),
--- src/structures/ClientUser.js
+++ src/structures/ClientUser.js
@@ -1,4 +1,5 @@
 const User = require('./User')
+const { StickerBuilder } = require("instagram-private-api/dist/sticker-builder/sticker-builder")
 
 /**
  * Represents the logged in client's Instagram user.
@@ -42,16 +43,78 @@ class ClientUser extends User {
      * @param {string} content The new biography
      * @returns {Promise<string>} The new biography
      */
-    async setBiography (content) {
+    async setBiography ({ content }) {
         this.biography = content
         await this.client.ig.account.setBiography(content)
         return this.biography
     }
 
-    async test(message) {
-        console.log(message)
-        return true
+    async postPhotoInStory ({ buffer, media }) {
+        const sticker = new StickerBuilder()
+            .add(StickerBuilder.attachmentFromMedia((media).center()))
+            .build()
+    
+        const story = await this.client.ig.publish.story({
+            file: buffer,
+            stickerConfig: sticker
+        })
+    
+        return story
     }
+    
+    async postStory ({ buffer }) {
+        const story = await this.client.ig.publish.story({
+            file: buffer
+        })
+    
+        return story
+    }
+    
+    async postPhoto ({ buffer, caption, location, usertags }) {
+        const media = await this.client.ig.publish.photo({
+            file: buffer,
+            caption: caption ?? "",
+            location: location ?? undefined,
+            usertags: usertags ?? undefined
+        })
+    
+        return media
+    }
+    
+    async findLocation ({ lat, lng, query }) {
+        const locations = await this.client.ig.search.location(lat, lng, query ?? "")
+        return locations
+    }
+    
+    async tagUser (users) {
+        async function generateUsertagFromName(ig, name, x, y) {
+            const clamp = (value, min, max) => Math.max(Math.min(value, max), min)
+    
+            x = clamp(x, 0.0001, 0.9999)
+            y = clamp(y, 0.0001, 0.9999)
+            const { pk } = await ig.user.searchExact(name)
+            return {
+                user_id: pk,
+                position: [x, y]
+            }
+        }
+    
+        if (!Array.isArray(users)) return []
+    
+        const usertags = []
+    
+        for (let i = 0; i < users.length; i++) {
+            const user = users[i]
+    
+            if (!user.username || !user.posX || !user.posY) continue
+    
+            const generatedUserTag = await generateUsertagFromName(this.client.ig, user.username, user.posX, user.posY)
+            usertags.push(generatedUserTag)
+        }
+    
+        return usertags
+    }
+    
 
     toJSON () {
         return {
