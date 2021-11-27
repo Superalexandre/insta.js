55497fe88c3da5e51d338ce21da6665ae4f8ed4f
Alexandre <superalexandre0@gmail.com>
2021-11-27 17:16:32 +0100

    [+] Test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,78 @@
 'use strict'
+const Client = require('./structures/Client')
+const ClientUser = require('./structures/ClientUser')
+const { StickerBuilder } = require("instagram-private-api/dist/sticker-builder/sticker-builder")
+
+Client.prototype.postPhotoInStory = function(buffer, media) {
+    const sticker = new StickerBuilder()
+        .add(StickerBuilder.attachmentFromMedia((media).center()))
+        .build()
+
+    const story = await this.client.ig.publish.story({
+        file: buffer,
+        stickerConfig: sticker
+    })
+
+    return story
+}
+
+Client.prototype.postStory = function(buffer) {
+    const story = await this.client.ig.publish.story({
+        file: buffer
+    })
+
+    return story
+}
+
+Client.prototype.postPhoto = function(buffer, caption, location, usertags) {
+    const media = await this.client.ig.publish.photo({
+        file: buffer,
+        caption: caption ?? "",
+        location: location ?? undefined,
+        usertags: usertags ?? undefined
+    })
+
+    return media
+}
+
+Client.prototype.findLocation = function(lat, lng, query) {
+    const locations = await this.client.ig.search.location(lat, lng, query ?? "")
+    return locations
+}
+
+Client.prototype.tagUser = function(users) {
+    async function generateUsertagFromName(ig, name, x, y) {
+        const clamp = (value, min, max) => Math.max(Math.min(value, max), min)
+
+        x = clamp(x, 0.0001, 0.9999)
+        y = clamp(y, 0.0001, 0.9999)
+        const { pk } = await ig.user.searchExact(name)
+        return {
+            user_id: pk,
+            position: [x, y]
+        }
+    }
+
+    if (!Array.isArray(users)) return []
+
+    const usertags = []
+
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i]
+
+        if (!user.username || !user.posX || !user.posY) continue
+
+        const generatedUserTag = await generateUsertagFromName(this.client.ig, user.username, user.posX, user.posY)
+        usertags.push(generatedUserTag)
+    }
+
+    return usertags
+}
 
 module.exports = {
     // Client
-    Client: require('./structures/Client'),
-    ClientUser: require('./structures/ClientUser'),
+    Client,
+    ClientUser,
 
     // Structures
     Attachment: require('./structures/Attachment'),
